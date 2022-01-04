bfdfc0ee54a9fd07700b00eb54da88c46c8544b9
Anthony Howell <anthony@howell-it.com>
2022-01-04 08:12:31 -0800

    adding cicd

---
+++ repoScripts/publish.ps1
@@ -0,0 +1,21 @@
+param (
+    [switch]$Test,
+    [string]$RelativePath = './'
+)
+
+$baseDir = Get-Item $RelativePath
+foreach ($manifest in (Get-ChildItem $RelativePath -Filter manifest.txt -Recurse)) {
+    
+    # Determine namespace based on folder structure
+    $rPath = $manifest.FullName.Replace($baseDir.FullName,'').Trim('\/')
+    $namespace = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
+    Write-Host "----------------------------------------------"
+    Write-Host "Found: '$rPath', publishing as '$namespace'..."
+    
+    # Publish the Action
+    if ($Test.IsPresent) {
+        ./runway.bin -q -N build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt"
+    } else {
+        ./runway.bin -q -N build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt" -p $namespace.ToLower()
+    }
+}
