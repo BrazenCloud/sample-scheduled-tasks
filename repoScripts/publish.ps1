7beb993d518c08d981f49c189858225e0c6d2e11
Anthony Howell <anthony@howell-it.com>
2022-10-31 10:02:48 -0700

    adding server

--- repoScripts/publish.ps1
+++ repoScripts/publish.ps1
@@ -1,21 +1,22 @@
 param (
     [switch]$Test,
-    [string]$RelativePath = './'
+    [string]$RelativePath = './',
+    [string]$Server = 'portal.brazencloud.com'
 )
 
 $baseDir = Get-Item $RelativePath
 foreach ($manifest in (Get-ChildItem $RelativePath -Filter manifest.txt -Recurse)) {
     
     # Determine namespace based on folder structure
-    $rPath = $manifest.FullName.Replace($baseDir.FullName,'').Trim('\/')
+    $rPath = $manifest.FullName.Replace($baseDir.FullName, '').Trim('\/')
     $namespace = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
     Write-Host "----------------------------------------------"
     Write-Host "Found: '$rPath', publishing as '$namespace'..."
     
     # Publish the Action
     if ($Test.IsPresent) {
-        ./runway.bin -q -N build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt"
+        ./runway.bin -q -N -S $Server build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt"
     } else {
-        ./runway.bin -q -N build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt" -p $namespace.ToLower()
+        ./runway.bin -q -N -S $Server build -it $manifest.FullName -o "$($namespace.Replace(':','-')).apt" -p $namespace.ToLower()
     }
 }
